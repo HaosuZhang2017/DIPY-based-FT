f82c4de83997b3e20a48312e15017e7237e08337
HaosuZhang2017 <33150239+HaosuZhang2017@users.noreply.github.com>
2024-01-31 22:29:42 +0100

    tracking_deterministic.py

--- tracking_deterministic.py
+++ tracking_deterministic.py
@@ -92,13 +92,97 @@ if has_fury:
     if interactive:
         window.show(scene)
 
-###############################################################################
-# .. figure:: tractogram_deterministic_dg.png
-#    :align: center
-# 
-#    **Corpus Callosum using deterministic maximum direction getter**
 
-###############################################################################
-# .. include:: ../links_names.inc
-# 
+
+#TMS atlas by Crimi
+
+import nibabel as nib
+atlas = nib.load(folder+"/atlas_dti.nii.gz")
+
+#  atlas = nib.load('aal_dti.nii.gz')
+labels = atlas.get_fdata()
+labelsint = labels.astype("int16")
+
+#Load labels
+
+labels_df = pd.read_csv(atlas_path+"/aal_labels.csv",sep="\t")
+
+# labels_df = pd.read_csv("aal_labels.txt",sep="\t")
+affine =np.eye(4)
+M, grouping = utils.connectivity_matrix(streamlines, affine, labelsint,
+                                        return_mapping=True,
+                                        mapping_as_streamlines=True)
+
+#set the data matrix
+M = M[1:,1:]
+
+# U-fibers are on the diagonal, set them to zero
+np.fill_diagonal(M,0)
+
+#Save connectivity matrix
+M_df = pd.DataFrame(M,index=labels_df["Region"],columns=labels_df["Region"])
+M_df.to_csv(folder+"/connMatrix.txt",sep="\t")
+
+# M_df.to_csv("connMatrix.txt",sep="\t")
+
+#Plot and save connectivity matrix
+import numpy as np
+plt.imshow(np.log1p(M), interpolation='nearest')
+#  plt.savefig("connectivity.png")
+
+plt.savefig(folder + "/connectivity.png")
+
+#Calculate CenterOfMass
+label_fields = []
+label_center = []
+for i in np.arange(1,labelsint.max()+1):
+    temp_mask = np.zeros(labelsint.shape)
+    temp_mask[labelsint == i] = 1
+    label_fields.append(labels_df["Region"][i-1])
+    if np.isnan(center_of_mass(temp_mask)[0]) == True or np.isnan(center_of_mass(temp_mask)[1]) == True or np.isnan(center_of_mass(temp_mask)[1]) == True :
+        c_of_m = 'Nan'
+    else:
+        c_of_m = str(int(center_of_mass(temp_mask)[0])) + "," + str(int(center_of_mass(temp_mask)[1])) + "," + str(int(center_of_mass(temp_mask)[2]) )
+        
+    label_center.append(c_of_m)
+    
+aal_coords = pd.DataFrame([label_fields,label_center],index=["Label","Center_of_Mass"])
+aal_coords = aal_coords.transpose()
+aal_coords.to_csv(folder + "/aal_coords.txt",sep="\t",index=False)
+
+# aal_coords.to_csv( "aal_coords.txt",sep="\t",index=False)
+
+tms = nib.load(folder + "/tms_points_dti.nii.gz")
+
+# tms = nib.load("tms_dti.nii.gz")
+
+tms_data = tms.get_fdata()
+tms_data = label(tms_data)
+
+#Calculate CenterOfMass
+label_counter = []
+label_fields = []
+label_center = []
+for i in np.arange(1,tms_data.max()+1):
+    label_counter.append(i)
+    aal_label = labelsint[tms_data == i][0]
+    if aal_label == 0:
+        label_fields.append("Background")
+    else:
+        label_fields.append(labels_df["Region"][aal_label-1])
+    coords = np.where(tms_data == i)
+    coords_str = str(coords[0][0]) + "," + str(coords[1][0]) + "," + str(coords[2][0])
+    label_center.append(coords_str)
+    
+aal_coords = pd.DataFrame([label_counter,label_fields,label_center],index=["TMS_Point","Label","Center_of_Mass"])
+aal_coords = aal_coords.transpose()
+aal_coords.to_csv(folder + "/tms_coords.txt",sep="\t",index=False)
+
+print (folder + ' is done!')
+
+# aal_coords.to_csv("tms_coords.txt",sep="\t",index=False)
+
+
+
+
 
